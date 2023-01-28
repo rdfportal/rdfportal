2565323668d2cc5c1405d3b25a716a27d138e439
Daisuke Satoh <dsatoh@kamonohashi.co.jp>
2023-01-28 15:14:07 +0900

    initial commit

---
+++ lib/rdf/portal.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "portal/version"
+
+module Rdf
+  module Portal
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rdf/portal_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rdf::Portal do
+  it "has a version number" do
+    expect(Rdf::Portal::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
